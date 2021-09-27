9681640f6e3047bad6f668fe8ebac12f51efbead
Ebbe A <ebbe.arildsson@gmail.com>
2021-09-27 19:58:35 +0200

    Made progressbar

--- js/indexcode.js
+++ js/indexcode.js
@@ -1,1 +1,21 @@
 const setTheme = theme => document.documentElement.className = theme;
+
+function setsidebar() {
+    let sidebar = document.getElementById("sidebar");
+    let button =  document.getElementById("btn");
+    let btntext = document.getElementById("btntext");
+    let content = document.getElementById("content");
+
+    if(sidebar.style.width != "250px"){
+        sidebar.style.width = "250px";
+        button.style.marginLeft = "200px";
+        btntext.style.transform = "rotate(45deg)";
+        content.display = "block";
+    } 
+    else {
+        sidebar.style.width = "0px";
+        button.style.marginLeft = "0px";
+        btntext.style.transform ="none";
+        content.display = "none";
+    }
+}
