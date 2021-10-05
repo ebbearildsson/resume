3555298ee88adcd769d01806c11b32af4d89b559
ebbearildsson <ebbe.arildsson@gmail.com>
2021-10-05 10:43:15 +0200

    ✨ Added theme switcher

--- js/indexcode.js
+++ js/indexcode.js
@@ -1,21 +1,27 @@
 const setTheme = theme => document.documentElement.className = theme;
 
+function setLang(lang){
+    let hideelement = document.getElementsByClassName(lang == 'sv' ? 'en' : 'sv');
+    let showelement = document.getElementsByClassName(lang);
+    for(let i = 0; i < hideelement.length; i ++){
+        hideelement[i].style.display = "none";
+        showelement[i].style.display = "inline"
+    }
+}
+
 function setsidebar() {
     let sidebar = document.getElementById("sidebar");
     let button =  document.getElementById("btn");
     let btntext = document.getElementById("btntext");
-    let content = document.getElementById("content");
 
     if(sidebar.style.width != "250px"){
         sidebar.style.width = "250px";
         button.style.marginLeft = "200px";
         btntext.style.transform = "rotate(45deg)";
-        content.display = "block";
     } 
     else {
         sidebar.style.width = "0px";
         button.style.marginLeft = "0px";
         btntext.style.transform ="none";
-        content.display = "none";
     }
 }
