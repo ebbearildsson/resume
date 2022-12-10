0393b1ac72d4eb84dee18dcec298e173b3247768
Ebbe Arildsson <ebbe.arildsson@gmail.com>
2022-12-10 20:07:29 +0100

    ✨ Dark mode!

--- js/pointsandlines.js
+++ js/pointsandlines.js
@@ -1,12 +1,23 @@
 let points = []
 let l = 300
+let bg = 255
 const s = 0.5
 const N = 50
 
 function setup() {
     var canvas = createCanvas(windowWidth, windowHeight * 0.5)
     canvas.parent('sketch')
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        bg = 0
+        stroke(255)
+    } 
+    else {
+        bg = 255
+        stroke(0)
+    }
+
     l = Math.floor(width / 6)
+
     for(let i = 0; i < N; i++) 
         points.push([createVector(random(width), random(height)), createVector(random(-s, s), random(-s, s))])
     points.push([createVector(-l/2, 0), createVector(0, 1)])
@@ -16,8 +27,9 @@ function setup() {
 }
 
 function draw() {
-    background(255)
-    stroke(0)
+    background(bg)
+    
+
     for(let i = 0; i < points.length; i++) {
         points[i][0].add(points[i][1])
         if(points[i][0].x > width || points[i][0].x < 0) points[i][1].x *= -1
