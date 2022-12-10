2d356d8decce2e4ea8b53c3abb07548c99371e9f
Ebbe Arildsson <ebbe.arildsson@gmail.com>
2022-12-10 14:54:00 +0100

    🩹 Moved text down sligthly in portrait

--- js/pointsandlines.js
+++ js/pointsandlines.js
@@ -7,7 +7,8 @@ function setup() {
     var canvas = createCanvas(windowWidth, windowHeight * 0.5)
     canvas.parent('sketch')
     l = Math.floor(width / 6)
-    for(let i = 0; i < N; i++) points.push([createVector(random(width), random(height)), createVector(random(-s, s), random(-s, s))])
+    for(let i = 0; i < N; i++) 
+        points.push([createVector(random(width), random(height)), createVector(random(-s, s), random(-s, s))])
     points.push([createVector(-l/2, 0), createVector(0, 1)])
     points.push([createVector(-l/2, height), createVector(0, -1)])
     points.push([createVector(width + l/2, 0), createVector(0, 1)])
@@ -21,10 +22,12 @@ function draw() {
         points[i][0].add(points[i][1])
         if(points[i][0].x > width || points[i][0].x < 0) points[i][1].x *= -1
         if(points[i][0].y > height || points[i][0].y < 0) points[i][1].y *= -1
-        points.forEach(p => {if(p != points[i] && dist(p[0].x, p[0].y, points[i][0].x, points[i][0].y) < l) line(p[0].x, p[0].y, points[i][0].x, points[i][0].y)})
+        points.forEach(p => {
+            if(p != points[i] && dist(p[0].x, p[0].y, points[i][0].x, points[i][0].y) < l) line(p[0].x, p[0].y, points[i][0].x, points[i][0].y)
+        })
     }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-  }
+}
